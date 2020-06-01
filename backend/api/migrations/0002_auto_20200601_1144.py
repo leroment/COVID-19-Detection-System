f2ac814c50f68541a7be6b35ec00a6a93e490f5f
Tim Porritt <tim.m.porritt@gmail.com>
2020-06-01 15:36:19 +1000

    Fixed migration

--- backend/api/migrations/0002_auto_20200601_1144.py
+++ backend/api/migrations/0002_auto_20200601_1144.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='diagnosis',
             name='creation_date',
-            field=models.DateTimeField(auto_now_add=True, default=1),
+            field=models.DateTimeField(auto_now_add=True),
             preserve_default=False,
         ),
         migrations.AddField(
