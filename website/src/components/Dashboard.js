33ca5b5d448e6a865f0baccf735ce2c3ec5acc49
Tim Porritt <tim.m.porritt@gmail.com>
2020-06-09 00:58:30 +1000

    Janky health officer buttons

--- website/src/components/Dashboard.js
+++ website/src/components/Dashboard.js
@@ -14,6 +14,8 @@ import {
   Card,
   CardHeader,
   CardContent,
+  Typography,
+  Box,
 } from "@material-ui/core";
 import { grey } from "@material-ui/core/colors";
 import NewDiagnosis from "./NewDiagnosis";
@@ -47,28 +49,21 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(diagnosisId, healthOfficer, status, dateSubmitted) {
-  return { diagnosisId, healthOfficer, status, dateSubmitted };
-}
-
-const rows = [
-  // createData("342341233124", "Steve Jobs", "Pending", "12/05/2020"),
-  // createData("342341233124", "Bill Gates", "Pending", "03/02/2020"),
-  // createData("342341233126", "Steve Balmer", "Result Ready", "14/03/2020"),
-  // createData("123323124423", "Steve Wozniak", "Submitted", "12/05/2020"),
-];
-
 export default function Dashboard() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [diagnoses, setDiagnoses] = useState([]);
 
-  useEffect(() => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const isStaff = user.is_staff;
+
+  const fetchDiagnoses = () => {
+    const url = isStaff
+      ? `http://127.0.0.1:8000/api/healthofficers/${user.id}/diagnoses/?status=AWAITING_REVIEW`
+      : `http://127.0.0.1:8000/api/users/${user.id}/diagnoses/`;
+
     axios
-      .get(
-        `http://127.0.0.1:8000/api/users/${localStorage.getItem(
-          "userId"
-        )}/diagnoses/`,
+      .get(url,
         {
           headers: {
             Authorization: `Token ${localStorage.getItem("token")}`,
@@ -81,32 +76,32 @@ export default function Dashboard() {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  };
+
+  useEffect(fetchDiagnoses, []);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const callback = (value) => {
+  const newDiagnosisCreatedCallback = (value) => {
     setOpen(false);
+    fetchDiagnoses();
+  };
 
-    axios
-      .get(
-        `http://127.0.0.1:8000/api/users/${localStorage.getItem(
-          "userId"
-        )}/diagnoses/`,
-        {
-          headers: {
-            Authorization: `Token ${localStorage.getItem("token")}`,
-          },
-        }
-      )
-      .then((response) => {
-        setDiagnoses(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const approve = (id, isApproved) => {
+    axios.put(
+      `http://127.0.0.1:8000/api/healthofficers/${user.id}/diagnoses/${id}/`,
+      {
+        "approved": isApproved,
+        "comment": prompt("Comment"),
+      },
+      {
+        headers: {
+          Authorization: `Token ${localStorage.getItem("token")}`,
+        },
+      }
+    ).then(fetchDiagnoses);
   };
 
   return (
@@ -158,36 +153,73 @@ export default function Dashboard() {
                   New Diagnosis +
                 </Button>
               }
-              title="Current Diagnoses"
+              title={isStaff ? "Diagnoses Awaiting Review" : "Current Diagnoses"}
             />
-            <NewDiagnosis open={open} parentCallback={callback} />
+            <NewDiagnosis open={open} parentCallback={newDiagnosisCreatedCallback} />
             <Divider />
             <CardContent>
               <Table className={classes.table}>
                 <TableHead>
                   <TableRow>
                     <TableCell>Diagnosis ID</TableCell>
-                    {/* <TableCell>Health Officer Assigned</TableCell>
+                    <TableCell>Health Officer Assigned</TableCell>
                     <TableCell>Status</TableCell>
-                    <TableCell>Date Submitted</TableCell> */}
+                    <TableCell>Date Submitted</TableCell>
+                    {isStaff && (
+                      <>
+                        <TableCell>Result</TableCell>
+                        <TableCell>Result Confidence</TableCell>
+                        <TableCell/>
+                        <TableCell/>
+                      </>
+                    )}
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {/* {rows.map((row, index) => (
-                    <TableRow key={index}>
-                      <TableCell component="th" scope="row">
-                        {row.diagnosisId}
-                      </TableCell>
-                      <TableCell align="left">{row.healthOfficer}</TableCell>
-                      <TableCell align="left">{row.status}</TableCell>
-                      <TableCell align="left">{row.dateSubmitted}</TableCell>
-                    </TableRow>
-                  ))} */}
                   {diagnoses.map((diagnosis, index) => (
                     <TableRow key={index}>
-                      <TableCell component="th" scope="row">
+                      <TableCell>
                         {diagnosis.id}
                       </TableCell>
+                      <TableCell align="left">{`${diagnosis.health_officer.first_name} ${diagnosis.health_officer.last_name}`}</TableCell>
+                      <TableCell align="left">{diagnosis.status}</TableCell>
+                      <TableCell align="left">{new Date(diagnosis.creation_date).toLocaleString()}</TableCell>
+                      { isStaff && (
+                        <>
+                          <TableCell align="left">
+                            <Typography component="div">
+                              <Box fontWeight="fontWeightBold">
+                                {diagnosis.result.has_covid ? "Has COVID-19" : "Does not have COVID-19"}
+                              </Box>
+                            </Typography>
+                          </TableCell>
+                          <TableCell align="left">
+                            {`${diagnosis.result.confidence * 100}%`}
+                          </TableCell>
+                          <TableCell align="right">
+                            <Button
+                              color="primary"
+                              size="small"
+                              variant="outlined"
+                              onClick={() => approve(diagnosis.id, true)}
+                              className={classes.actionButton}
+                            >
+                              Approve
+                            </Button>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Button
+                              color="secondary"
+                              size="small"
+                              variant="outlined"
+                              onClick={() => approve(diagnosis.id, false)}
+                              className={classes.actionButton}
+                            >
+                              Decline
+                            </Button>
+                          </TableCell>
+                        </>
+                      )}
                     </TableRow>
                   ))}
                 </TableBody>
--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -79,6 +79,7 @@ function Login(props) {
             .then((response) => {
               localStorage.setItem("token", response.data.token);
               localStorage.setItem("userId", response.data.user.id);
+              localStorage.setItem("user", JSON.stringify(response.data.user));
               setRedirectToDashboard(true);
             })
             .catch((err) => {
